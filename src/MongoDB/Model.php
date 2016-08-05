c7394eba153c212f559bc228e8c0b5f9f8364937
felix <felix@ruzzoli.de>
2016-08-05 20:04:06 +0200

    some much needed cleanup

--- src/MongoDB/Model.php
+++ src/MongoDB/Model.php
@@ -24,6 +24,8 @@ class Model extends \MongoDB\Collection
     protected $_id;
     protected $_attributes;
 
+    ////// Bootstrapping
+
     public static function collection($collectionName = '')
     {
         $client = Di::getDefault()->getShared('dispatcher')->getParam('client');
@@ -40,14 +42,210 @@ class Model extends \MongoDB\Collection
         return (new static(Di::getDefault()->getShared('mongo'), $dbname, $collectionName));
     }
 
-    public static function mongoTime()
+
+    ////// Basics
+
+    /* Old Style CRUD Functions
+    public function save()
     {
-        return new UTCDateTime(round(microtime(true) * 1000) . '');
+        if ($attributes != null) {
+            $this->fill($attributes);
+        }
+        $this->event('beforeSave');
+        if (isset($this->_id)) {
+            $this->event('beforeUpdate');
+            $this->updateOne(['_id' => $this->_id], ['$set' => $this->_attributes]);
+            $this->event('afterUpdate');
+        } else {
+            $this->event('beforeCreate');
+            $insertResult = $this->insertOne($this->_attributes);
+            $this->_id = $insertResult->getInsertedId();
+            $this->event('afterCreate');
+        }
+        $this->event('afterSave');
+        return $this;
     }
 
-    public function getId($asString = true)
+    public function delete()
     {
-        return $asString ? (string)$this->_id : $this->_id;
+        $this->event('beforeDelete');
+        $this->deleteOne(['_id' => $this->getId(false)]);
+        $this->event('afterDelete');
+        return $this;
+    }
+
+
+    public function update(array $attributes)
+    {
+        $this->event('beforeSave');
+        $this->event('beforeUpdate');
+        $this->fill($attributes);
+        static::collection()->updateOne(['_id' => $this->_id], ['$set' => $attributes]);
+        $this->event('afterUpdate');
+        $this->event('afterSave');
+        return $this;
+    }
+
+    public function insert($entry)
+    {
+        return static::collection()->insertOne($entry);
+    }
+
+    */
+    /// NOTES:
+    // using static::collectin() means the the Model and thereby the Collection is chosen implicitly based upon the Classname used in the call
+
+    public static function get(array $filter = [], array $options = [])
+    {
+        return static::collection()->find($filter, $options);
+    }
+
+    public static function create(array $data)
+    {
+        $data['_id'] = (string) (new ObjectID());
+        $result = static::collection()->insertOne($data);
+        return $result;
+    }
+
+    public static function getById($id)
+    {
+        $collection = static::collection();
+        $result = $collection->findOne(['_id' => $id]);
+        if ($result == null)
+        {
+            throw new \MemMaker\MongoDB\Exceptions\EntryNotFoundException(vsprintf("Entry with id '%1\$s' not found in collection '%2\$s'", [$id, $collection->getCollectionName()]));
+        }
+        return $result;
+    }
+
+    public static function replaceById($id, array $data)
+    {
+        $result = static::collection()->replaceOne(['_id' => $id], $data);
+        return $result;
+    }
+
+    public static function deleteById($id)
+    {
+        return static::collection()->deleteOne(['_id' => $id]);
+    }
+
+    ///// Explicit Model Methods (needed for Entries Class)
+
+    public static function getByModel(string $model, array $filter = [], array $options = [])
+    {
+        return static::collection($model)->find($filter, $options);
+    }
+
+    public static function createForModel(string $modelName, array $entry = [])
+    {
+        $entry['_id'] = (string) (new ObjectID());
+        $modelObject = static::collection($modelName);
+        $modelObject->insertOne($entry);
+    }
+
+    public static function getByModelAndId(string $modelName, string $entryid)
+    {
+        $modelObject = static::collection($modelName);
+        return $modelObject->findOne(['_id' => $entryid]);
+    }
+
+    public static function replaceByModelId(string $modelName, string $entryid, array $inputdata)
+    {
+        $modelObject = static::collection($modelName);
+        return $modelObject->replaceOne(['_id' => $entryid], $inputdata);
+    }
+
+    public static function deleteByModelId(string $modelName, string $entryid)
+    {
+        $modelObject = static::collection($modelName);
+        return $modelObject->deleteOne(['_id' => $entryid]);
+    }
+
+
+    ////// Advanced
+
+    public function increment($argument, $value = 1)
+    {
+        $this->{$argument} += $value;
+        $this->updateOne(['_id' => $this->_id], ['$set' => [$argument => $this->{$argument}]]);
+        return $this;
+    }
+
+    public function decrement($argument, $value = 1)
+    {
+        $this->{$argument} -= $value;
+        $this->updateOne(['_id' => $this->_id], ['$set' => [$argument => $this->{$argument}]]);
+        return $this;
+    }
+
+    public static function mapReduce($mapJS, $reduceJS, array $query = array())
+    {
+        $map = new Javascript($mapJS);
+        $reduce = new Javascript($reduceJS);
+
+        $source = static::getSource();
+        $command = new Command([
+            "mapreduce" => $source,
+            "map" => $map,
+            "reduce" => $reduce,
+            "query" => new \stdClass(), //new BSONDocument($query),
+            "out" => 'results'
+        ]);
+
+        $manager = Di::getDefault()->get('mongo');
+        $results = $manager->executeCommand('TheBackend', $command);
+
+        return $results;
+    }
+
+    public static function getFullTextSearchQuery($model, $searchString, $searchLimit = 500)
+    {
+        $searchFields = array();
+        foreach ($model['searchFields'] as $fieldname) {
+            $searchFields[] = array($fieldname => new \MongoRegex('/' . $searchString . '/iu'));
+        }
+        return
+            array(
+                array('$or' => $searchFields),
+                'sort' => $model['sortFieldOrder'],
+                'limit' => $searchLimit
+            );
+    }
+
+    public static function getWithReferences(string $model, array $filter = [], array $options = [])
+    {
+        $modelObject = static::collection($model);
+        if (!$modelObject->hasRelations($model))
+        {
+            return self::getByModel($model, $filter, $options);
+        }
+        $relations = $modelObject->getRelations();
+        $pipeline = [];
+        if (count($filter) > 0)
+        {
+            $pipeline[] = static::getMatchPipeline($filter);
+        }
+
+        foreach ($relations as $relation)
+        {
+            $pipeline[] = static::getLookUpPipeline($relation['name'], $relation['referencedModel'], 'local_'.$relation['name']);
+        }
+        return $modelObject->aggregate($pipeline);
+    }
+
+    protected static function getMatchPipeline($query)
+    {
+        return ['$match' => $query];
+    }
+
+    protected static function getLookUpPipeline($localFieldname, $refCollectionName, $asLocalFieldname)
+    {
+        return ['$lookup' => [
+            'from' => $refCollectionName,
+            'localField' => $localFieldname,
+            'foreignField' => "_id",
+            'as' => $asLocalFieldname
+        ]];
     }
 
     public static function destroyDatabase()
@@ -58,28 +256,45 @@ class Model extends \MongoDB\Collection
         $db->drop();
     }
 
-    public function unsetField($field)
+
+    ////// Complex (currently not integrated)
+
+
+    /* Assigns all fields it can find in $data to the $this model object.
+    /* Tries to parse everything correct according to fieldtype.  */
+    public function assign($data)
     {
-        $path = explode('.', $field);
-        $lastPart = end($path);
-        if (count($path) > 1) {
-            $ref = $this->getAttrRef($field, 1);
-        } else {
-            $ref = &$this->_attributes;
-        }
-        if ($ref != false) {
-            $type = gettype($ref);
-            if ($type == 'object' && isset($ref->{$lastPart})) {
-                unset($ref->{$lastPart});
-            } else if ($type == 'array' && isset($ref[$lastPart])) {
-                unset($ref[$lastPart]);
-            } else {
-                return false;
+        $modelclassname = get_class($this);
+        $model = $modelclassname::getModel();
+
+        foreach ($model['fields'] as $fieldname => $fieldFlags)
+        {
+            if (!array_key_exists($fieldname, $data))
+            {
+                // no value for this fieldtype
+                continue;
             }
-            $this->updateOne(['_id' => $this->_id], ['$unset' => [$field => '']]);
-            return true;
+
+            $fieldtype = $fieldFlags['type'];
+
+            $this->$fieldname = array_key_exists('default', $fieldFlags) ? $fieldFlags['default'] : '';
+
+            $valueConverter = Di::getDefault()->getShared('valueConverter');
+
+            $convertedValue = $valueConverter->getConversion($data[$fieldname], $fieldtype);
+
+            $this->setField($fieldname, $convertedValue);
         }
-        return false;
+    }
+
+    public function setField($fieldname, $value)
+    {
+        if ($this->$fieldname !== $value && (! in_array($fieldname, $this->changedFields)))
+        {
+            // only add to changedFields if the value changed and the field is not already in there
+            $this->changedFields[] = $fieldname;
+            $this->$fieldname = $value;
+        }
     }
 
     protected function castArrayAttributes(array $data, $useMutators = false)
@@ -119,77 +334,28 @@ class Model extends \MongoDB\Collection
         return $value;
     }
 
-    protected function event($name)
+    ////// Convenience
+
+    public static function mongoTime()
     {
-        if (method_exists($this, $name)) {
-            $this->{$name}();
-        }
+        return new UTCDateTime(round(microtime(true) * 1000) . '');
     }
 
-    protected function getAttrRef($path, $rightOffset = 0)
-    {
-        $path = explode('.', $path);
-        $length = count($path) - $rightOffset;
-        $return = &$this->_attributes;
-        for ($i = 0; $i <= $length - 1; ++$i) {
-            if (isset($return->{$path[$i]})) {
-                if ($i == $length - 1) {
-                    return $return->{$path[$i]};
-                } else {
-                    $return = &$return->{$path[$i]};
-                }
-            } else if (isset($return[$path[$i]])) {
-                if ($i == $length - 1) {
-                    return $return[$path[$i]];
-                } else {
-                    $return = &$return[$path[$i]];
-                }
-            } else {
-                return false;
-            }
-        }
-        return $return;
-    }
-
-    public function __get($name)
-    {
-        $methodName = 'get' . Text::camelize($name);
-        return isset($this->_attributes[$name]) ? (method_exists($this, $methodName) ? $this->{$methodName}($this->_attributes[$name]) : $this->_attributes[$name])
-            : (isset($this->_relations[$name]) ? $this->_relations[$name]
-                : (isset(static::$relations[$name]) ? $this->loadRelation($name) : null));
-    }
-
-    public function __set($name, $value)
-    {
-        $methodName = 'set' . Text::camelize($name);
-        $this->_attributes[$name] = method_exists($this, $methodName) ? $this->{$methodName}($value) : $this->castAttribute($name, $value);
-    }
+    //////// Essentials
 
     public function __toString()
     {
         return json_encode($this->toArray());
     }
 
-    public static function mapReduce($mapJS, $reduceJS, array $query = array())
+    //////// Events
+
+    protected function event($name)
     {
-        $map = new Javascript($mapJS);
-        $reduce = new Javascript($reduceJS);
-
-        $source = static::getSource();
-        $command = new Command([
-            "mapreduce" => $source,
-            "map" => $map,
-            "reduce" => $reduce,
-            "query" => new \stdClass(), //new BSONDocument($query),
-            "out" => 'results'
-        ]);
-
-        $manager = Di::getDefault()->get('mongo');
-        $results = $manager->executeCommand('TheBackend', $command);
-
-        return $results;
+        if (method_exists($this, $name)) {
+            $this->{$name}();
+        }
     }
-
 
     public function beforeCreate()
     {
@@ -217,169 +383,6 @@ class Model extends \MongoDB\Collection
 
     public function beforeUpdate()
     {
-
         $this->updated_at = self::mongoTime();
     }
-
-    public static function getFullTextSearchQuery($model, $searchString, $searchLimit = 500)
-    {
-        $searchFields = array();
-        foreach ($model['searchFields'] as $fieldname) {
-            $searchFields[] = array($fieldname => new \MongoRegex('/' . $searchString . '/iu'));
-        }
-        return
-            array(
-                array('$or' => $searchFields),
-                'sort' => $model['sortFieldOrder'],
-                'limit' => $searchLimit
-            );
-    }
-
-    /* Assigns all fields it can find in $data to the $this model object.
-    /* Tries to parse everything correct according to fieldtype.  */
-    public function assign($data)
-    {
-        $modelclassname = get_class($this);
-        $model = $modelclassname::getModel();
-
-        foreach ($model['fields'] as $fieldname => $fieldFlags)
-        {
-            if (!array_key_exists($fieldname, $data))
-            {
-                // no value for this fieldtype
-                continue;
-            }
-
-            $fieldtype = $fieldFlags['type'];
-
-            $this->$fieldname = array_key_exists('default', $fieldFlags) ? $fieldFlags['default'] : '';
-
-            $valueConverter = Di::getDefault()->getShared('valueConverter');
-
-            $convertedValue = $valueConverter->getConversion($data[$fieldname], $fieldtype);
-
-            $this->setField($fieldname, $convertedValue);
-        }
-    }
-
-    public function setField($fieldname, $value)
-    {
-        if ($this->$fieldname !== $value && (! in_array($fieldname, $this->changedFields)))
-        {
-            // only add to changedFields if the value changed and the field is not already in there
-            $this->changedFields[] = $fieldname;
-            $this->$fieldname = $value;
-        }
-    }
-
-
-    public function increment($argument, $value = 1)
-    {
-        $this->{$argument} += $value;
-        $this->updateOne(['_id' => $this->_id], ['$set' => [$argument => $this->{$argument}]]);
-        return $this;
-    }
-
-    public function decrement($argument, $value = 1)
-    {
-        $this->{$argument} -= $value;
-        $this->updateOne(['_id' => $this->_id], ['$set' => [$argument => $this->{$argument}]]);
-        return $this;
-    }
-
-    public function save()
-    {
-        if ($attributes != null) {
-            $this->fill($attributes);
-        }
-        $this->event('beforeSave');
-        if (isset($this->_id)) {
-            $this->event('beforeUpdate');
-            $this->updateOne(['_id' => $this->_id], ['$set' => $this->_attributes]);
-            $this->event('afterUpdate');
-        } else {
-            $this->event('beforeCreate');
-            $insertResult = $this->insertOne($this->_attributes);
-            $this->_id = $insertResult->getInsertedId();
-            $this->event('afterCreate');
-        }
-        $this->event('afterSave');
-        return $this;
-    }
-
-    public static function get(string $model = '', array $filter = [], array $options = [])
-    {
-        return static::collection($model)->find($filter, $options);
-    }
-
-    protected static function getMatchPipeline($query)
-    {
-        return ['$match' => $query];
-    }
-
-    protected static function getLookUpPipeline($localFieldname, $refCollectionName, $asLocalFieldname)
-    {
-        return ['$lookup' => [
-            'from' => $refCollectionName,
-            'localField' => $localFieldname,
-            'foreignField' => "_id",
-            'as' => $asLocalFieldname
-        ]];
-    }
-
-    public function insert($entry)
-    {
-        return static::collection()->insertOne($entry);
-    }
-
-    public function delete()
-    {
-        $this->event('beforeDelete');
-        $this->deleteOne(['_id' => $this->getId(false)]);
-        $this->event('afterDelete');
-        return $this;
-    }
-
-
-    public function update(array $attributes)
-    {
-        $this->event('beforeSave');
-        $this->event('beforeUpdate');
-        $this->fill($attributes);
-        static::collection()->updateOne(['_id' => $this->_id], ['$set' => $attributes]);
-        $this->event('afterUpdate');
-        $this->event('afterSave');
-        return $this;
-    }
-
-    public static function replaceById($id, array $data)
-    {
-        $result = static::collection()->replaceOne(['_id' => $id], $data);
-        return $result;
-    }
-
-    public static function create(array $data)
-    {
-        $data['_id'] = (string) (new ObjectID());
-        $result = static::collection()->insertOne($data);
-        return $result;
-    }
-
-    public static function getById($id)
-    {
-        $collection = static::collection();
-        $result = $collection->findOne(['_id' => $id]);
-        if ($result == null)
-        {
-            throw new \MemMaker\MongoDB\Exceptions\EntryNotFoundException(vsprintf("Entry with id '%1\$s' not found in collection '%2\$s'", [$id, $collection->getCollectionName()]));
-        }
-        return $result;
-    }
-
-    public static function deleteById($id)
-    {
-        return static::collection()->deleteOne(['_id' => $id]);
-    }
-
-
 }
